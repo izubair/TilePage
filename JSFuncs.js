f13e9973f46ca5a1024cbe4e2aec78dfdb6c30d2
izubair <irfanzub@hotmail.com>
2017-06-05 09:24:27 -0700

    javascript code in separate file
    javascript code in separate file

---
+++ JSFuncs.js
@@ -0,0 +1,29 @@
+﻿function initEvents()
+{
+    $("#myspan").click(function () {
+        alert('REQUEST Service');
+    });
+    $("#myspan1").click(function () {
+        alert('ASK A Question');
+    });
+    $("#myspan2").click(function () {
+        alert('MAP Service Requests');
+    });
+}
+var loggedIn = false;
+function mainpageOnLoad() {
+    if (loggedIn) {
+        $("#aLogon").html("wideball");
+        $("#aLogout").show();
+    }
+    else {
+        $("#aLogon").html("Login/Register");
+        $("#aLogout").hide();
+    }
+}
+    
+        
+
+
+
+         
